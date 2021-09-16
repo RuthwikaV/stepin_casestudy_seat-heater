901250f25a4eeea621c8900916565b0beb5e877e
RuthwikaV <89633636+RuthwikaV@users.noreply.github.com>
2021-09-16 23:01:56 +0530

    Add files via upload

---
+++ 3_Implementation/src/Act1.c
@@ -0,0 +1,8 @@
+#include "act1.h"
+void seat_sensing(){
+    DDRD|=(1<<PD2); //Set D2= 1
+    DDRD&=~(1<<PD3); //Clear bit
+    PORTD|=(1<<PD3); //Set bit
+    DDRD&=~(1<<PD4); //Clear bit
+    PORTD|=(1<<PD4);
+ }    //Set bit
